32d02ac460ce27a79ef9d1301f74a9d7bf8d8b98
Hawk Hyouck Kim <hawk.kim1974@gmail.com>
2019-01-19 11:32:45 +0900

    first commit

--- app/blinky.c
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx_hal.h"
-#include "gpio.h"
-#include "blinky.h"
-
-void
-blinky_init(void)
-{
-}
--- app/blinky.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BLINKY_DEF_H__
-#define __BLINKY_DEF_H__
-
-extern void blinky_init(void);
-
-#endif /* !__BLINKY_DEF_H__ */
